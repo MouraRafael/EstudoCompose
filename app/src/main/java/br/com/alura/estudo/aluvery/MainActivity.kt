65b3f3e72fc3713e9af5b4e5b1a226b67a09f792
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-23 14:56:51 -0300

    1-2-05     Faça como eu fiz: modificando design dos Composables

--- app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
@@ -5,16 +5,24 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.alura.estudo.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,54 +40,83 @@ class MainActivity : ComponentActivity() {
 fun MyFirstComposable() {
     AluveryTheme {
         Surface {
-            Text(text = "Meu compose")
-            Text(text = "Meu Composito")
+           CustomLayout()
         }
     }
 }
 
 
-
 @Preview(showBackground = true)
 @Composable
-fun ColumnPreview(){
+fun ColumnPreview() {
     Column {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun RowPreview(){
+fun RowPreview() {
     Row {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
     }
 }
+
 @Preview(showBackground = true)
 @Composable
-fun BoxPreview(){
+fun BoxPreview() {
     Box {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
     }
 }
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun CustomLayout(){
-    Column {
+fun CustomLayout() {
+    Column(
+        Modifier
+            .padding(all = 8.dp)
+            .background(color = Color.Blue)
+            .padding(all = 8.dp)
+            .fillMaxWidth()
+            .fillMaxHeight()
+
+    ) {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
-        Row {
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 8.dp, vertical = 16.dp)
+                .background(Color.Green)
+                .fillMaxWidth(0.5f)
+
+        ) {
             Text(text = "Texto 3")
             Text(text = "Texto 4")
         }
-        Box{
-            Row {
+        Box(
+            modifier = Modifier
+                .padding(horizontal = 8.dp, vertical = 16.dp)
+                .background(Color.Red)
+
+        ) {
+            Row(
+                Modifier
+                    .padding(horizontal = 8.dp, vertical = 16.dp)
+                    .background(Color.Cyan)
+                    .fillMaxWidth()
+            ) {
                 Text(text = "Texto 5")
                 Text(text = "Texto 6")
             }
-            Column {
+            Column(
+                Modifier
+                    .padding(horizontal = 8.dp, vertical = 16.dp)
+                    .background(Color.Yellow)
+            ) {
                 Text(text = "Texto 7")
                 Text(text = "Texto 8")
             }
