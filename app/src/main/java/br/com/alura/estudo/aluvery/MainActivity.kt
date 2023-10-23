de7cdbd24615c86c73b9db50ec8e95dd9e00c684
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-23 16:18:19 -0300

    1-2-07     Faça como eu fiz: implementando o item do produto

--- app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
@@ -1,125 +1,47 @@
 package br.com.alura.estudo.aluvery
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import br.com.alura.estudo.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyFirstComposable()
+            ItemComposable()
         }
     }
 
 
 }
-
-@Composable
-fun MyFirstComposable() {
-    AluveryTheme {
-        Surface {
-           CustomLayout()
-        }
-    }
-}
-
 
 @Preview(showBackground = true)
 @Composable
-fun ColumnPreview() {
+fun ItemComposable(){
     Column {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RowPreview() {
-    Row {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun BoxPreview() {
-    Box {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-    }
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun CustomLayout() {
-    Column(
-        Modifier
-            .padding(all = 8.dp)
-            .background(color = Color.Blue)
-            .padding(all = 8.dp)
-            .fillMaxWidth()
-            .fillMaxHeight()
-
-    ) {
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
-        Row(
-            modifier = Modifier
-                .padding(horizontal = 8.dp, vertical = 16.dp)
-                .background(Color.Green)
-                .fillMaxWidth(0.5f)
-
-        ) {
-            Text(text = "Texto 3")
-            Text(text = "Texto 4")
-        }
         Box(
-            modifier = Modifier
-                .padding(horizontal = 8.dp, vertical = 16.dp)
-                .background(Color.Red)
-
-        ) {
-            Row(
-                Modifier
-                    .padding(horizontal = 8.dp, vertical = 16.dp)
-                    .background(Color.Cyan)
-                    .fillMaxWidth()
-            ) {
-                Text(text = "Texto 5")
-                Text(text = "Texto 6")
-            }
-            Column(
-                Modifier
-                    .padding(horizontal = 8.dp, vertical = 16.dp)
-                    .background(Color.Yellow)
-            ) {
-                Text(text = "Texto 7")
-                Text(text = "Texto 8")
-            }
-        }
+            Modifier
+                .width(50.dp)
+                .height(50.dp)
+                .background(Color.Blue)
+        )
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
+        Text(text = "Texto1")
+        Text(text = "Texto2")
     }
 }
