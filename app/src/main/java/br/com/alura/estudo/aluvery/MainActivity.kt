bdbad20fe9257a2eb465aaaeab5c69fdb2f7f540
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-23 16:29:42 -0300

    1-2-08     Desafio: implementando um design

--- app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -43,5 +45,26 @@ fun ItemComposable(){
         Image(painter = painterResource(id = R.drawable.ic_launcher_background), contentDescription = null)
         Text(text = "Texto1")
         Text(text = "Texto2")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DesafioLayout(){
+
+    Row(
+        Modifier.height(150.dp)
+    ){
+        Box(modifier = Modifier
+            .fillMaxHeight()
+            .width(100.dp)
+            .background(Color.Blue)
+        ){}
+        Column() {
+            Text(text = "Test 1",Modifier.fillMaxWidth()
+                .background(Color(0xFFededed))
+                .padding(16.dp))
+            Text(text = "Test 2",Modifier.padding(16.dp))
+        }
     }
 }
