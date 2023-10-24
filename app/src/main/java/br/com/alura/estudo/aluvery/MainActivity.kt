0c8852bc18ba9d9bd7b8eb53078796e5e089490d
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-24 14:43:02 -0300

    1-3-05     Ajustando os textos

--- app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,14 +20,19 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import br.com.alura.estudo.aluvery.ui.theme.Purple200
 import br.com.alura.estudo.aluvery.ui.theme.Purple40
 import br.com.alura.estudo.aluvery.ui.theme.Purple500
@@ -63,18 +69,35 @@ fun ItemComposable() {
                         )
                     )
                 )
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-            Modifier
-                .offset(y=(-50).dp)
-                .size(100.dp)
-                .clip(shape = CircleShape)
-                .align(CenterHorizontally)
-        )
-        Text(text = "Texto1")
-        Text(text = "Texto2")
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null,
+                Modifier
+                    .offset(y = 50.dp)
+                    .size(100.dp)
+                    .clip(shape = CircleShape)
+                    .align(BottomCenter)
+            )
+        }
+        Spacer(modifier = Modifier.height(50.dp))
+
+        Column(Modifier.padding(16.dp)) {
+            Text(
+                text = LoremIpsum(50).values.first(),
+                fontSize = 18.sp,
+                fontWeight = FontWeight(700),
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+
+            )
+            Text(
+                text = "R$ 15,99",
+                fontSize = 14.sp,
+                fontWeight = FontWeight(400),
+                modifier= Modifier.padding(top = 8.dp)
+            )
+        }
     }
 }
 
