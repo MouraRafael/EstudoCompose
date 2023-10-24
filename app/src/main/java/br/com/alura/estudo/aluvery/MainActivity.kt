07b415c1df68ad083272f9d01766fd8692b3d128
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-24 15:21:34 -0300

    1-3-07     Finalizando o item do produto

--- app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/MainActivity.kt
@@ -12,11 +12,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -53,55 +56,63 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ItemComposable() {
-    Column(
-        Modifier
-            .height(250.dp)
-            .width(200.dp)
-    ) {
-        Box(
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        shape= RoundedCornerShape(15.dp),
+        shadowElevation = 4.dp) {
+        Column(
             Modifier
-                .fillMaxWidth()
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple500, Teal200
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+
+        ) {
+            val imageSize= 80.dp
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
                         )
                     )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .offset(y = (imageSize/2))
+                        .size(imageSize)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
                 )
-        ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_launcher_background),
-                contentDescription = null,
-                Modifier
-                    .offset(y = 50.dp)
-                    .size(100.dp)
-                    .clip(shape = CircleShape)
-                    .align(BottomCenter)
-            )
-        }
-        Spacer(modifier = Modifier.height(50.dp))
+            }
+            Spacer(modifier = Modifier.height(imageSize/2))
 
-        Column(Modifier.padding(16.dp)) {
-            Text(
-                text = LoremIpsum(50).values.first(),
-                fontSize = 18.sp,
-                fontWeight = FontWeight(700),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
 
-            )
-            Text(
-                text = "R$ 15,99",
-                fontSize = 14.sp,
-                fontWeight = FontWeight(400),
-                modifier= Modifier.padding(top = 8.dp)
-            )
+                )
+                Text(
+                    text = "R$ 15,99",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400),
+                    modifier= Modifier.padding(top = 8.dp)
+                )
+            }
         }
+
     }
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
 fun DesafioLayout() {
 
