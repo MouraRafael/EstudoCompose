eba01886f1e4b6c89a370e8d750cf8e483bc8374
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-26 16:36:40 -0300

    2-3-05     Faça como eu fiz: implementando o TextField

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
@@ -1,9 +1,12 @@
 package br.com.alura.estudo.aluvery.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,25 +16,34 @@ import br.com.alura.estudo.aluvery.sampledata.sampleSections
 import br.com.alura.estudo.aluvery.ui.components.ProductsSection
 
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(sections:Map<String,List<Product>>) {
-    LazyColumn(
-        Modifier
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        contentPadding = PaddingValues(vertical = 16.dp)
 
-    ) {
+    Column {
+        var valor = ""
+        OutlinedTextField(value = valor, onValueChange = {newValue->
+            valor+=newValue
+        })
+        LazyColumn(
+            Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(vertical = 16.dp)
 
-        for (section in sections){
-            val title = section.key
-            val products = section.value
-            item{
-                ProductsSection(title = title, products = products)
+        ) {
+
+            for (section in sections) {
+                val title = section.key
+                val products = section.value
+                item {
+                    ProductsSection(title = title, products = products)
+                }
             }
+
+
         }
-
-
     }
 }
 
