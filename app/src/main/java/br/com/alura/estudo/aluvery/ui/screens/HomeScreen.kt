5c6aa0e4309a71e5940d5231775db4405dfdab13
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-27 15:54:41 -0300

    2-6-08     Faça como eu fiz: a última refatoração

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
@@ -4,18 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -29,6 +22,7 @@ import br.com.alura.estudo.aluvery.sampledata.sampleProducts
 import br.com.alura.estudo.aluvery.sampledata.sampleSections
 import br.com.alura.estudo.aluvery.ui.components.CardProductItem
 import br.com.alura.estudo.aluvery.ui.components.ProductsSection
+import br.com.alura.estudo.aluvery.ui.components.SearchTextField
 import br.com.alura.estudo.aluvery.ui.theme.AluveryTheme
 
 
@@ -36,16 +30,8 @@ import br.com.alura.estudo.aluvery.ui.theme.AluveryTheme
 @Composable
 fun HomeScreen(sections: Map<String, List<Product>>,
                searchText:String = "") {
-    var valor: String by remember {
+    var text: String by remember {
         mutableStateOf(searchText)
-    }
-
-    val produtos = remember(valor){
-    //Este remember faz com que a filtro só seja executado novamente caso haja uma mudança na variavel valor
-        if(valor.isNotBlank()){sampleProducts.filter { p ->
-            p.name.contains(valor, ignoreCase = true) ||
-                    p.description?.contains(valor, ignoreCase = true) ?: false
-        }}else emptyList()
     }
 
 
@@ -54,20 +40,23 @@ fun HomeScreen(sections: Map<String, List<Product>>,
 
     Column {
 
-        OutlinedTextField(
-            value = valor,
-            onValueChange = { newValue ->
-                valor = newValue
-            },
-            Modifier
-                .padding(16.dp)
-                .fillMaxWidth(),
-            shape = RoundedCornerShape(100),
-            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Ícone de busca")},
-            label = { Text(text = "Produtos")},
-            placeholder = { Text(text = "O que você procura?")}
 
-        )
+
+        SearchTextField(
+            searchText = text,
+            onSearchTextChanged = {
+            text =it
+        })
+
+
+
+        val produtos = remember(text){
+            //Este remember faz com que a filtro só seja executado novamente caso haja uma mudança na variavel valor
+            if(text.isNotBlank()){sampleProducts.filter { p ->
+                p.name.contains(text, ignoreCase = true) ||
+                        p.description?.contains(text, ignoreCase = true) ?: false
+            }}else emptyList()
+        }
         LazyColumn(
             Modifier
                 .fillMaxSize(),
@@ -75,7 +64,7 @@ fun HomeScreen(sections: Map<String, List<Product>>,
             contentPadding = PaddingValues(bottom = 16.dp)
 
         ) {
-            if(valor.isBlank()){
+            if(text.isBlank()){
                 for (section in sections) {
                     val title = section.key
                     val products = section.value
