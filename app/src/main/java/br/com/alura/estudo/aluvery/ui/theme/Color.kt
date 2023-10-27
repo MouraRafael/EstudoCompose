88afd59f3833dd3d9ef25b6aca80219a46db37e6
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-27 11:54:09 -0300

    2-5-03     Faça como eu fiz: adicionando o card de produto

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/theme/Color.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/theme/Color.kt
@@ -7,6 +7,8 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 
+val PrimaryVariant = Color(0xFF808CED)
+
 val Teal200 = Color(0xFF03DAC5)
 
 val PurpleGrey80 = Color(0xFFCCC2DC)
