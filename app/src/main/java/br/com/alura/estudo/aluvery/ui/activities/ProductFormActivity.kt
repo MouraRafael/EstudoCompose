2ba6cfc4e3fffcc20d0550ced2b53262f6ccd4f7
Rafael Moura <rafael.g.moura@hotmail.com>
2023-10-30 12:10:12 -0300

    3-1-07     Faça como eu fiz: implementando os composables

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/activities/ProductFormActivity.kt
@@ -3,8 +3,17 @@ package br.com.alura.estudo.aluvery.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.alura.estudo.aluvery.ui.theme.AluveryTheme
 
 class ProductFormActivity: ComponentActivity() {
@@ -16,10 +25,51 @@ class ProductFormActivity: ComponentActivity() {
         setContent{
             AluveryTheme {
                 Surface {
-                    Text(text = "ASDF")
+                    ProductFormScreen()
                 }
             }
         }
 
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductFormScreen() {
+
+    Column {
+
+        Text(text = "Criando o Produto")
+
+        var url by remember  { mutableStateOf("") }
+        TextField(value = url, onValueChange = {
+            url = it
+        })
+
+        var name by remember  { mutableStateOf("") }
+        TextField(value = name, onValueChange = {
+            name = it
+        })
+
+        var price by remember  { mutableStateOf("") }
+        TextField(value = price, onValueChange = {
+            price = it
+        })
+
+
+        var description by remember  { mutableStateOf("") }
+        TextField(value = description, onValueChange = {
+            description = it
+        })
+
+
+
+    }
+    
+}
+
+@Preview
+@Composable
+fun ProductFormScreenPreview() {
+    ProductFormScreen()
 }
