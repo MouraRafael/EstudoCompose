fe2353d5ff217294d6e71ca07d13dbc3a16a2dad
Rafael Moura <rafael.g.moura@hotmail.com>
2023-11-08 13:51:34 -0300

    4-2-06     Faça como eu fiz: utilizando o Saveable nas telas

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/screens/HomeScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,7 +47,7 @@ class HomeScreenUiState(
 
 @Composable
 fun HomeScreen(products: List<Product>) {
-    var text by remember {
+    var text by rememberSaveable {
         mutableStateOf("")
     }
 
