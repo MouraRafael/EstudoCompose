e9cb9c612ad337c0c4beb5e49283027828ed5387
Rafael Moura <rafael.g.moura@hotmail.com>
2023-11-08 12:27:01 -0300

    4-2-03     Faça como eu fiz: mantendo o estado do Lazy Layout

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/components/CardProductItem.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/components/CardProductItem.kt
@@ -16,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -41,7 +42,7 @@ fun CardProductItem(
     elevation: Dp = 4.dp,
     expanded:Boolean = false
 ) {
-    var expandedState by remember {
+    var expandedState by rememberSaveable {
         mutableStateOf(expanded)
     }
     Card(
