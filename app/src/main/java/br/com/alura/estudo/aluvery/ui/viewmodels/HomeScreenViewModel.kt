22a234b9f204a43259d10a45b33b4ab4106b22a4
Rafael Moura <rafael.g.moura@hotmail.com>
2023-11-09 11:48:53 -0300

    4-4-07     Faça como eu fiz: utilizando StateFlow no DAO

--- app/src/main/java/br/com/alura/estudo/aluvery/ui/viewmodels/HomeScreenViewModel.kt
+++ app/src/main/java/br/com/alura/estudo/aluvery/ui/viewmodels/HomeScreenViewModel.kt
@@ -5,24 +5,20 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import br.com.alura.estudo.aluvery.dao.ProductDao
 import br.com.alura.estudo.aluvery.model.Product
 import br.com.alura.estudo.aluvery.sampledata.sampleCandies
 import br.com.alura.estudo.aluvery.sampledata.sampleDrinks
 import br.com.alura.estudo.aluvery.sampledata.sampleProducts
 import br.com.alura.estudo.aluvery.ui.states.HomeScreenUiState
+import kotlinx.coroutines.launch
 
 class HomeScreenViewModel : ViewModel() {
     private val dao = ProductDao()
 
 
-    var uiState:HomeScreenUiState by mutableStateOf(HomeScreenUiState(
-        sections = mapOf(
-            "Todos os Produtos" to dao.products(),
-            "Promoções" to sampleDrinks + sampleCandies,
-            "Doces" to sampleCandies,
-            "bebidas" to sampleDrinks
-        ),
+    var uiState: HomeScreenUiState by mutableStateOf(HomeScreenUiState(
         onSearchTextChanged = {
             uiState = uiState.copy(
 
@@ -33,12 +29,23 @@ class HomeScreenViewModel : ViewModel() {
     ))
         private set
 
+    init {
+        viewModelScope.launch {
+            dao.products().collect { products ->
+                uiState = uiState.copy(
+                    sections = mapOf(
+                        "Todos os Produtos" to products,
+                        "Promoções" to sampleDrinks + sampleCandies,
+                        "Doces" to sampleCandies,
+                        "bebidas" to sampleDrinks
+                    )
+                )
+            }
+        }
+    }
 
 
-
-
-
-    fun containsInNameOrDescription(text:String) = { p: Product ->
+    fun containsInNameOrDescription(text: String) = { p: Product ->
         p.name.contains(
             text,
             ignoreCase = true
@@ -47,8 +54,9 @@ class HomeScreenViewModel : ViewModel() {
             ignoreCase = true
         ) ?: false
     }
-    fun searchedProducts(text:String) = if (text.isNotBlank()) {
-        sampleProducts.filter(containsInNameOrDescription(text))
+
+    fun searchedProducts(text: String) = if (text.isNotBlank()) {
+        dao.products().value.filter(containsInNameOrDescription(text))
     } else emptyList()
 
 
